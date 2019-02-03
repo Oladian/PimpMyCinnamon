2da59726f91c4d54556264e711b3d785a22921fd
Ismael Arias <ismaelariasmn@gmail.com>
2019-02-03 21:38:18 +0100

    Now working, there was a problem executing dconf and gsettings commands as sudo. Still looking how to change menu icon using gsettings command.

--- configurator.sh
+++ configurator.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 #Download and install theme and icons
-wget -P /tmp/ https://cinnamon-spices.linuxmint.com/files/themes/Adapta-Nokto.zip && unzip /tmp/Adapta-Nokto.zip && mv /tmp/Adapta-Nokto /usr/share/themes/
-wget -P /tmp/ https://launchpad.net/~papirus/+archive/ubuntu/papirus-dev/+build/16346002/+files/papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb -O papirus.deb && dpkg -i /tmp/papirus.deb
-
+wget -P /tmp/ https://cinnamon-spices.linuxmint.com/files/themes/Adapta-Nokto.zip && unzip /tmp/Adapta-Nokto.zip -d /tmp/ && mv /tmp/Adapta-Nokto /usr/share/themes/
+wget -O /tmp/papirus.deb https://launchpad.net/~papirus/+archive/ubuntu/papirus-dev/+build/16346002/+files/papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb && DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/papirus.deb 
 #Change wallpaper solid grey
-gsettings set org.cinnamon.desktop.background primary-color '#111111111111'
-gsettings set org.cinnamon.desktop.background picture-options 'none'
-
+su $SUDO_USER -c "gsettings set org.cinnamon.desktop.background primary-color '#111111111111'"
+su $SUDO_USER -c "gsettings set org.cinnamon.desktop.background picture-options 'none'"
 #Change theme and icons
-gsettings set org.cinnamon.desktop.wm.preferences theme "Adapta-Nokto"
-gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark-Grey"
-gsettings set org.cinnamon.theme name "Mint-Y-Dark-Grey"
-gsettings set org.cinnamon.desktop.interface icon-theme "Papirus-Dark"
-
+su $SUDO_USER -c "gsettings set org.cinnamon.desktop.wm.preferences theme 'Adapta-Nokto'"
+su $SUDO_USER -c "gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Grey'"
+su $SUDO_USER -c "gsettings set org.cinnamon.theme name 'Mint-Y-Dark-Grey'"
+su $SUDO_USER -c "gsettings set org.cinnamon.desktop.interface icon-theme 'Papirus-Dark'"
 #Load keybindings for terminal
-dconf load /org/gnome/terminal/legacy/keybindings/ < mykeybindings
+su $SUDO_USER -c "dconf load /org/gnome/terminal/legacy/keybindings/ < mykeybindings"
