271b171d6a4289410bd931f07e4c916b650ac206
Ismael Arias <ismaelariasmn@gmail.com>
2019-02-04 21:07:08 +0100

    Now the script makes sure you execute it as root. Also it prompts an option to enable or not HiDPI scaling (useful for notebooks with fullHD resolution)

--- configurator.sh
+++ configurator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+	then echo "You should run the script as root!"
+	exit
+fi
+
 #Download and install theme and icons
 wget -P /tmp/ https://cinnamon-spices.linuxmint.com/files/themes/Adapta-Nokto.zip && unzip /tmp/Adapta-Nokto.zip -d /tmp/ && mv /tmp/Adapta-Nokto /usr/share/themes/
 wget -O /tmp/papirus.deb https://launchpad.net/~papirus/+archive/ubuntu/papirus-dev/+build/16346002/+files/papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb && DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/papirus.deb 
@@ -15,3 +20,11 @@ su $SUDO_USER -c "gsettings set org.cinnamon.desktop.interface icon-theme 'Papir
 su $SUDO_USER -c "dconf load /org/gnome/terminal/legacy/keybindings/ < resources/mykeybindings"
 #Load json with menu config (icon for menu and taskbar icons)
 cp resources/1.json ~/.cinnamon/configs/menu@cinnamon.org/1.json
+
+echo "Enable HiDPI scaling factor setting?"
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) su $SUDO_USER -c "gsettings set org.cinnamon.desktop.interface scaling-factor 2"; break;;
+		No ) exit;;
+	esac
+done
